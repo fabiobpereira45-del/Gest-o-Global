bb066b65f22d04fac690b556958d3fbd8af63171
Fabio4134 <fabio.bpereira40@gmail.com>
2026-04-08 17:04:19 -0300

    fix: completely remove process.env references for supabase public keys to bypass false undefined injection during nextjs vercel builds

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,9 +4,12 @@ import { cookies } from 'next/headers'
 export async function createClient() {
     const cookieStore = await cookies()
 
+    const supabaseUrl = 'https://vevivqcluyutlatwsnjh.supabase.co'
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZldml2cWNsdXl1dGxhdHdzbmpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NTU2MTgsImV4cCI6MjA5MTIzMTYxOH0.te2bkSjxjU5ZQ5tzGPAInKaHjc--KX4lOL_M1vgYlNg'
+
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        supabaseUrl,
+        supabaseKey,
         {
             cookies: {
                 getAll() {
